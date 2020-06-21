d9c32eb0e738840fb65e2c4e31e7a58c1741c876
Harley Bates <Harley.Bates@kpmg.co.uk>
2020-06-21 14:57:58 +0100

    notes

--- sunsetSunrise/controller.py
+++ sunsetSunrise/controller.py
@@ -4,12 +4,13 @@ from datetime import datetime,timedelta
 import requests, os
 
 class Controller:
- 
+
     def getLocation(self, input_location):
-        location = Nominatim(timeout=5).geocode(input_location, language='en_US')        
+        location = Nominatim(timeout=5).geocode(input_location, language='en_US')
         return location
-   
+
     def getSunriseReport(self, data, location):
+        # Date comes out in the wrong format
         date = data['date']
 
         latitude = str(location.latitude)
@@ -18,9 +19,10 @@ class Controller:
         sunriseSunset = []
         response = requests.get("https://api.sunrise-sunset.org/json?lat="+latitude+"&lng="+longitude+"&date="+date)
         jsonResponse = response.json()
+        # Change both to GMT
         sunrise = jsonResponse['results']['sunrise']
         sunset = jsonResponse['results']['sunset']
-        report = sunriseReport(date, sunrise, sunset)        
+        report = sunriseReport(date, sunrise, sunset)
 
         sunriseSunset.append(report)
         return sunriseSunset
